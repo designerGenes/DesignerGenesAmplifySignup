f64ec270681621de6ae72df0a59940bcf1a3f11f
designerjeans <jnationdesignerjeans@gmail.com>
2024-05-01 18:14:54 -0500

    add Amplify

---
+++ DesignerGenesSignup/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  DesignerGenesSignup
+//
+//  Created by Jaden Nation on 5/1/24.
+//
+
+import Foundation
+import AWSAPIPlugin
+import AWSDataStorePlugin
+import Amplify
+
+class DataManager {
+    
+}
