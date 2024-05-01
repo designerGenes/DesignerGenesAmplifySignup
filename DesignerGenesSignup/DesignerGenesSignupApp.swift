e24fd6bbf1e7c9e86e7d04f4998dc000d94be84c
designerjeans <jnationdesignerjeans@gmail.com>
2024-05-01 17:41:31 -0500

    initial commit

---
+++ DesignerGenesSignup/DesignerGenesSignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignerGenesSignupApp.swift
+//  DesignerGenesSignup
+//
+//  Created by Jaden Nation on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignerGenesSignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
