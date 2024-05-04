31a45f4ca21644562ab27cbfbf38cddf9b34bbe2
designerjeans <jnationdesignerjeans@gmail.com>
2024-05-04 09:11:16 -0500

    connect to Cognito user pool

--- DesignerGenesSignup/DesignerGenesSignupApp.swift
+++ DesignerGenesSignup/DesignerGenesSignupApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSCognitoAuthPlugin
+
 
 @main
 struct DesignerGenesSignupApp: App {
+    init() {
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+        } catch {
+            fatalError("Could not configure Amplify")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             EntrySignupView()
         }
+        
     }
 }
--- DesignerGenesSignup/Views/EntrySignupView.swift
+++ DesignerGenesSignup/Views/EntrySignupView.swift
@@ -6,14 +6,35 @@
 //
 
 import SwiftUI
+import Amplify
+import Combine
 
 class EntrySignupViewModel: ObservableObject {
     @Published var username: String = ""
     @Published var email: String = ""
     @Published var password: String = ""
+    
+    func signup () async  {
+        let userAttributes = [AuthUserAttribute(.email, value: self.email)]
+        let options = AuthSignUpRequest.Options(userAttributes: userAttributes)
+        if let result = try? await Amplify.Auth.signUp(username: self.username, password: self.password, options: options) {
+            if result.isSignUpComplete {
+                postSignup()
+            }
+        } else {
+            print("Unable to sign up, ya dingus")
+        }
+    }
+    
+//    @MainActor
+    func postSignup() {
+        print("Signed up!")
+    }
 }
 
 struct EntrySignupView: View {
+    @State private var keyboardHeight: CGFloat = 0
+    @State private var bottomPadding: CGFloat = 0
     @ObservedObject private var viewModel: EntrySignupViewModel = EntrySignupViewModel()
     
     struct FieldModifier: ViewModifier {
@@ -21,16 +42,13 @@ struct EntrySignupView: View {
             content
                 .autocorrectionDisabled()
                 .tint(.black)
-            
                 .padding(.vertical, 4)
                 .padding(.horizontal, 8)
                 .background(Color.white)
                 .cornerRadius(5)
-                
-                
         }
     }
-    
+        
     private func MakeTextField(defaultValue: String, value: Binding<String>, isSecure: Bool = false) -> some View {
         if isSecure {
             return AnyView(SecureField(defaultValue, text: value)
@@ -42,49 +60,56 @@ struct EntrySignupView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(alignment: .center) {
-                    Spacer()
-                        .frame(height: 100)
-                    Image(systemName: "suit.spade.fill")
-                        .renderingMode(.template)
-                        .foregroundColor(.white)
-                        .font(.system(size: 150))
-                    Spacer()
-                        .frame(height: 200)
-                    HStack {
-                        VStack(alignment: .leading, spacing: 16) {
-                            MakeTextField(defaultValue: "Email", value: $viewModel.email)
-                            MakeTextField(defaultValue: "Username", value: $viewModel.username)
-                            MakeTextField(defaultValue: "Password", value: $viewModel.password, isSecure: true)
+            Form {
+                Section {
+                    VStack(alignment: .center) {
+                        Spacer()
+                            .frame(height: 100)
+                        Image(systemName: "suit.spade.fill")
+                            .renderingMode(.template)
+                            .foregroundStyle(Color(uiColor: .gray.withAlphaComponent(0.3)))
+                            .font(.system(size: 150))
+                        Spacer()
+                            .frame(height: 200)
+                        HStack {
+                            VStack(alignment: .leading, spacing: 16) {
+                                MakeTextField(defaultValue: "Email", value: $viewModel.email)
+                                MakeTextField(defaultValue: "Username", value: $viewModel.username)
+                                MakeTextField(defaultValue: "Password", value: $viewModel.password, isSecure: true)
+                            }
+                            Spacer()
                         }
-                        Spacer()
-                    }
-                    .padding(.horizontal)
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            
-                        }, label: {
-                            Text("Submit")
-                                .foregroundStyle(.black)
-                        })
                         .padding(.horizontal)
-                        .foregroundColor(.white)
-                        //                    .background(Color.black)
-                        .cornerRadius(5)
-                        .padding()
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                Task { @MainActor in
+                                    await self.viewModel.signup()
+                                }
+                                
+                            }, label: {
+                                Text("Submit")
+                                    .foregroundStyle(.black)
+                            })
+                            .padding(.horizontal)
+                            .foregroundColor(.white)
+                            .cornerRadius(5)
+                            .padding()
+                        }
                     }
                 }
             }
-            
             .background {
                 Color(uiColor: .lightGray.withAlphaComponent(0.2))
             }
-            .ignoresSafeArea()
+            .navigationTitle("Sign up")
+            .navigationBarTitleDisplayMode(.large)
+            .navigationBarHidden(false)
+            .navigationBarTitleDisplayMode(.large)
+            
+                        
         }
         .ignoresSafeArea()
-        
     }
 }
 
